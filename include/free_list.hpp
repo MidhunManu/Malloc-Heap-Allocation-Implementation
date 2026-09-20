b6ac11f64347bd44a241c3788be96220e9e66f23
MidhunManu <midhunmanu384@gmail.com>
2026-09-20 18:49:59 +0530

    add: free list APIs

--- include/free_list.hpp
+++ include/free_list.hpp
@@ -11,3 +11,10 @@ struct FreeList
 {
     BlockHeader* head;
 };
+
+void clearList(FreeList& list);
+void insertAtHead(FreeList& list, BlockHeader* head);
+void updateHeadPrev(FreeList& list, BlockHeader* block);
+void removeHead(FreeList& list);
+void removeNode(FreeList& list, BlockHeader* block);
+void removeTail(FreeListNode& list);
